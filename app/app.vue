9e58c86cc0f3ab739ffc3e12f741ee4c2053a1f2
RinatMadreiter <95545985+RinatMadreiter@users.noreply.github.com>
2025-09-09 13:23:31 +0200

    moved main css to app.vue for speed

--- app/app.vue
+++ app/app.vue
@@ -14,7 +14,39 @@
 </template>
 
 <style lang="css">
-@import url("~/assets/styles/main.css");
+body {
+    background-color: black;
+    color: white;
+    margin: 0;
+    overflow-x: hidden;
+}
+
+body, button, input, textarea {
+  font-family: 'Inter', sans-serif;
+}
+
+html {
+    scroll-behavior: smooth;
+}
+
+::-webkit-scrollbar {
+    width: 10px;
+}
+
+::-webkit-scrollbar-track {
+    background-color: #1A1A1A;
+}
+
+::-webkit-scrollbar-thumb {
+    border-radius: 8px;
+    background-color: #888;
+
+}
+
+::-webkit-scrollbar-thumb:hover {
+    background-color: #FA2759;
+    transform: scale(2);
+}
 
 .welcome-navigaion {
   height: 100vh;
